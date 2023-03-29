5c97cb3c1bcd9b25e0ef0958aeeabd66689006bc
Fredrik Wictorsson <wictorsson@gmail.com>
2023-03-29 18:44:22 +0200

    solve first step

--- pages/Components/ArtPiecesPreview/ArtPiecesPreview.jsx
+++ pages/Components/ArtPiecesPreview/ArtPiecesPreview.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+//import React from "react";
 import Image from "next/image";
 
 export default function ArtPiecesPreview({ image, title, artist }) {
   return (
     <div>
-      <Image scr={image} alt={"image"} width={230} height={140} />
+      <Image src={image} alt={"image"} width={200} height={200} />
       {artist} {title}
     </div>
   );
